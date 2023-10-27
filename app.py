a1d30010c1d8ca51a31056923283e0118005dad0
holowaczi <igor.holowacz@gmail.com>
2023-10-27 14:28:27 +0200

    added Setting, Simulation, Info tabs

--- app.py
+++ app.py
@@ -5,19 +5,25 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.H1(children='HeatBeamX. To będzie super projekt!!!', style={'textAlign':'center'}),
     html.Div([
-        dcc.Tabs(id ="setting_tabs",
-                children=[
-                    dcc.Tab(
-                        label="Tissue"
-                    ),
-                    dcc.Tab(
-                        label="ROI"
-                    ),
-                    dcc.Tab(
-                        label="Laser"
-                    )
-                ]
-        )
+        dcc.Tabs(id="main_tabs",
+            children=[
+                dcc.Tab(id="settings", label="Settings",
+                    children = [    
+                        dcc.Tabs(id ="setting_tabs",
+                                children=[
+                                    dcc.Tab(
+                                        label="Tissue"
+                                    ),
+                                    dcc.Tab(
+                                        label="ROI"
+                                    ),
+                                    dcc.Tab(
+                                        label="Laser"
+                                    )])
+                ]),
+                dcc.Tab(id="simulation",label="Simulation"),
+                dcc.Tab(id="info_tab",label="Info")
+            ])
     ])
 ])
 
