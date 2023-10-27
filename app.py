420a5a57316270e9023909c78fd24fa140dd5b4f
holowaczi <igor.holowacz@gmail.com>
2023-10-27 12:44:31 +0200

    added Tabs for settings

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='HeatBeamX. To będzie super projekt!!!', style={'textAlign':'center'}),
+    html.Div([
+        dcc.Tabs(id ="setting_tabs",
+                children=[
+                    dcc.Tab(
+                        label="Tissue"
+                    ),
+                    dcc.Tab(
+                        label="ROI"
+                    ),
+                    dcc.Tab(
+                        label="Laser"
+                    )
+                ]
+        )
+    ])
 ])
 
 if __name__ == '__main__':
