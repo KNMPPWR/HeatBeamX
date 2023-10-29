2c4d330bb782f6f05bfb165dfc53d93a58750b8a
holowaczi <igor.holowacz@gmail.com>
2023-10-29 19:28:51 +0100

    Calculating ROI size

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.layout = html.Div([
                                     dcc.Tab(
                                         label="ROI",
                                         children =[
-                                            html.Label("Voxel dimension: ",htmlFor="voxel_dim_input"),
+                                            html.Label("Voxel dimension (mm): ",htmlFor="voxel_dim_input"),
                                             dcc.Input(id="voxel_dim_input",type="number", min=0.001, step=0.001, value=1.000),
                                             html.Br(),
                                             html.Div(id="no_of_voxels",
@@ -28,7 +28,18 @@ app.layout = html.Div([
                                                         dcc.Input(id="no_of_voxel_y", type="number", min=1, step=1, value=60),
                                                         html.Label("No. of voxels in Z-direction: ",htmlFor="no_of_voxel_y"),
                                                         dcc.Input(id="no_of_voxel_z", type="number", min=1, step=1, value=60)
-                                                    ])
+                                                    ]),
+                                            html.Label("Dimensions of ROI", htmlFor="dim_of_roi"),
+                                            html.Div(id="dim_of_roi",
+                                                    children=[
+                                                        html.Label("X: ",htmlFor="dim_x"),
+                                                        dcc.Input(id="dim_x",value=60,disabled=True),
+                                                        html.Label("Y: ",htmlFor="dim_y"),
+                                                        dcc.Input(id="dim_y",value=60,disabled=True),
+                                                        html.Label("Z:",htmlFor="dim_z"),
+                                                        dcc.Input(id="dim_z",value=60,disabled=True)
+                                                    ]
+                                            )
                                         ]
                                     ),
                                     dcc.Tab(
@@ -40,6 +51,17 @@ app.layout = html.Div([
             ])
     ])
 ])
+
+@callback(
+    [Output("dim_x","value"),Output("dim_y","value"),Output("dim_z","value")],
+    Input("voxel_dim_input","value"),
+    Input("no_of_voxel_x","value"),
+    Input("no_of_voxel_y","value"),
+    Input("no_of_voxel_z","value"),
+)
+def calculate_dim(voxel_dim, no_of_voxel_x, no_of_voxel_y, no_of_voxel_z):
+    return round(voxel_dim*no_of_voxel_x,3), round(voxel_dim*no_of_voxel_y,3), round(voxel_dim*no_of_voxel_z,3)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
