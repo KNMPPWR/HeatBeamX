1c290538fd95b04fad36a7d8661d56737c80921d
holowaczi <igor.holowacz@gmail.com>
2023-11-30 09:43:29 +0100

    Visual chnges

--- app.py
+++
@@ -1,3 +0,0 @@
-from dash import Dash
-
-app = Dash(__name__)
--- views/info_view.py
+++
@@ -1,8 +0,0 @@
-from dash import html, dcc, callback, Input, Output, State
-import json
-
-from models.AppDataModel import AppDataModel
-
-
-def create_info_view(app_model: AppDataModel):
-    return dcc.Tab(id="info", label="Info", children=[])
